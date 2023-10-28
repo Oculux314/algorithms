25deb9ecb4a8e1d28d6e3082feaf89eacf30246f
Oculux314 <nate.williamson.nz@gmail.com>
2023-10-28 17:34:47 +1300

    :green_heart: add DynamicArray data structure

--- src/main/java/com/oculux/se284/datastructures/lists/DynamicArray.java
+++ src/main/java/com/oculux/se284/datastructures/lists/DynamicArray.java
@@ -1,8 +1,59 @@
 package com.oculux.se284.datastructures.lists;
 
-public class DynamicArray {
+public class DynamicArray implements List {
+
+  private int[] array;
+  private int size;
 
   public DynamicArray() {
-    System.out.println("DynamicArray constructor");
+    this(10);
+  }
+
+  public DynamicArray(int size) {
+    array = new int[size];
+    size = 0;
+  }
+
+  @Override
+  public void add(int value) {
+    if (size() == maxSize()) {
+      throw new UnsupportedOperationException("Resizing not implemented");
+    }
+
+    array[size()] = value;
+    size++;
+  }
+
+  @Override
+  public void set(int index, int value) {
+    if (index >= size()) {
+      throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+    }
+
+    array[index] = value;
+  }
+
+  @Override
+  public void remove() {
+    if (size() == 0) {
+      throw new UnsupportedOperationException("Cannot remove from empty list");
+    }
+
+    array[size() - 1] = 0;
+    size--;
+  }
+
+  @Override
+  public int get(int index) {
+    return array[index];
+  }
+
+  @Override
+  public int size() {
+    return size;
+  }
+
+  private int maxSize() {
+    return array.length;
   }
 }
--- src/test/java/com/oculux/se284/datastructures/lists/DynamicArrayTest.java
+++ src/test/java/com/oculux/se284/datastructures/lists/DynamicArrayTest.java
@@ -1,14 +1,44 @@
 package com.oculux.se284.datastructures.lists;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
 public class DynamicArrayTest {
 
   @Test
-  public void testAdd() {
+  public void testInitialise() {
     DynamicArray array = new DynamicArray();
-    assertTrue(true);
+    assertEquals(array.size(), 0);
+  }
+
+  @Test
+  public void testSizeAfterAdd() {
+    DynamicArray array = new DynamicArray();
+    array.add(1);
+    assertEquals(1, array.size());
+  }
+
+  @Test
+  public void testGetAfterAdd() {
+    DynamicArray array = new DynamicArray();
+    array.add(1);
+    assertEquals(1, array.get(0));
+  }
+
+  @Test
+  public void testSet() {
+    DynamicArray array = new DynamicArray();
+    array.add(1);
+    array.set(0, 2);
+    assertEquals(2, array.get(0));
+  }
+
+  @Test
+  public void testRemove() {
+    DynamicArray array = new DynamicArray();
+    array.add(1);
+    array.remove();
+    assertEquals(0, array.size());
   }
 }
