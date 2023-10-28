47271db1fdd849f4347979c847c0b27c3f262260
Oculux314 <nate.williamson.nz@gmail.com>
2023-10-28 15:35:07 +1300

    :tada: wipe all and initialise maven structure

---
+++ src/main/java/com/oculux/se284/datastructures/lists/DynamicArray.java
@@ -0,0 +1,8 @@
+package com.oculux.se284.datastructures.lists;
+
+public class DynamicArray {
+
+  public DynamicArray() {
+    System.out.println("DynamicArray constructor");
+  }
+}
---
+++ src/test/java/com/oculux/se284/datastructures/lists/DynamicArrayTest.java
@@ -0,0 +1,14 @@
+package com.oculux.se284.datastructures.lists;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DynamicArrayTest {
+
+  @Test
+  public void testAdd() {
+    DynamicArray array = new DynamicArray();
+    assertTrue(true);
+  }
+}
