00bad168beba00f40921f9089a7685fafce666f5
Oculux314 <nate.williamson.nz@gmail.com>
2023-10-28 17:01:52 +1300

    :blue_heart: add List interface

---
+++ src/main/java/com/oculux/se284/datastructures/lists/List.java
@@ -0,0 +1,9 @@
+package com.oculux.se284.datastructures.lists;
+
+public interface List {
+  void add(int value);
+  void set(int index, int value);
+  void remove();
+  int get(int index);
+  int size();
+}
