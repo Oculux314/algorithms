0cc5a9bb5c12a5d9bf83b2a64bf81fda46476be8
Oculux314 <nate.williamson.nz@gmail.com>
2023-11-10 13:08:24 +1300

    :test_tube: implement toString in BinarySearchTree

--- src/main/java/com/oculux/se284/datastructures/maps/binarytrees/BinarySearchTree.java
+++ src/main/java/com/oculux/se284/datastructures/maps/binarytrees/BinarySearchTree.java
@@ -1,31 +1,48 @@
 package com.oculux.se284.datastructures.maps.binarytrees;
 
+import com.oculux.se284.Untested;
 import com.oculux.se284.datastructures.maps.Map;
 import com.oculux.se284.datastructures.maps.NodeNotFoundException;
 
 public class BinarySearchTree implements Map {
 
-  private class Node {
-    private int key;
-    private int value;
-    private Node left;
-    private Node right;
-    private Node parent;
+  protected enum Branch {
+    LEFT,
+    RIGHT
+  }
 
-    private Node(int key, int value, Node parent) {
+  protected class Node {
+    int key;
+    int value;
+    Node left;
+    Node right;
+    Node parent;
+
+    Node(int key, int value, Node parent) {
       this.key = key;
       this.value = value;
       this.parent = parent;
       size++;
     }
+
+    @Override
+    public String toString() {
+      String leftString = (left == null) ? "" : left.toString();
+      String rightString = (right == null) ? "" : right.toString();
+      return String.format("(%s%s%s)", leftString, key, rightString);
+    }
   }
 
-  private Node root = null;
+  protected Node root = null;
   private int size = 0;
+
+  protected Node createNode(int key, int value, Node parent) {
+    return new Node(key, value, parent);
+  }
 
   public void put(int key, int value) {
     if (root == null) {
-      root = new Node(key, value, null);
+      root = createNode(key, value, null);
       return;
     }
 
@@ -42,9 +59,9 @@ public class BinarySearchTree implements Map {
 
       if (next == null) {
         if (key < node.key) {
-          node.left = new Node(key, value, node);
+          node.left = createNode(key, value, node);
         } else {
-          node.right = new Node(key, value, node);
+          node.right = createNode(key, value, node);
         }
         return;
       }
@@ -77,6 +94,40 @@ public class BinarySearchTree implements Map {
     }
 
     size--;
+  }
+
+  private Node getNextLarger(Node node) {
+    if (node.right != null) {
+      return getMinOfSubtree(node.right);
+    } else {
+      return getNextRightParent(node);
+    }
+  }
+
+  private Node getMinOfSubtree(Node node) {
+    while (node.left != null) {
+      node = node.left;
+    }
+    return node;
+  }
+
+  private Node getNextRightParent(Node node) {
+    while (node.parent != null && node.parent.left == node) {
+      node = node.parent;
+    }
+    return node.parent;
+  }
+
+  private void connectParentToChild(Node node, Node child) {
+    // Connects the specified node's parent to the child in the same direction (left/right)
+    // The child must be the only child of the node or null (if the node has no children)
+    if (node.parent == null) {
+      root = child;
+    } else if (node.parent.left == node) {
+      connectNodes(node.parent, Branch.LEFT, child);
+    } else {
+      connectNodes(node.parent, Branch.RIGHT, child);
+    }
   }
 
   @Override
@@ -119,36 +170,29 @@ public class BinarySearchTree implements Map {
     throw new NodeNotFoundException(key);
   }
 
-  private Node getNextLarger(Node node) {
-    if (node.right != null) {
-      return getMinOfSubtree(node.right);
-    } else {
-      return getNextRightParent(node);
-    }
-  }
-
-  private Node getMinOfSubtree(Node node) {
-    while (node.left != null) {
-      node = node.left;
-    }
-    return node;
-  }
-
-  private Node getNextRightParent(Node node) {
-    while (node.parent != null && node.parent.left == node) {
-      node = node.parent;
-    }
-    return node.parent;
-  }
-
-  private void connectParentToChild(Node node, Node child) {
-    // Only call on nodes with at most one child
-    if (node.parent == null) {
+  protected void connectNodes(Node parent, Branch connectionDirection, Node child) {
+    if (parent == null) {
       root = child;
-    } else if (node.parent.left == node) {
-      node.parent.left = child;
-    } else {
-      node.parent.right = child;
+      return;
     }
+
+    if (connectionDirection == Branch.LEFT) {
+      parent.left = child;
+    } else {
+      parent.right = child;
+    }
+
+    if (child != null) {
+      child.parent = parent;
+    }
+  }
+
+  @Override
+  public String toString() {
+    if (root == null) {
+      return "()";
+    }
+
+    return root.toString();
   }
 }
--- src/test/java/com/oculux/se284/datastructures/maps/binarytrees/BinarySearchTreeTests.java
+++ src/test/java/com/oculux/se284/datastructures/maps/binarytrees/BinarySearchTreeTests.java
@@ -1,12 +1,40 @@
 package com.oculux.se284.datastructures.maps.binarytrees;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.oculux.se284.datastructures.maps.Map;
 import com.oculux.se284.datastructures.maps.MapTests;
+import org.junit.jupiter.api.Test;
 
 public class BinarySearchTreeTests extends MapTests {
 
   @Override
   protected Map createMap() {
     return new BinarySearchTree();
+  }
+
+  private BinarySearchTree createTemplateTree() {
+    BinarySearchTree tree = (BinarySearchTree) createMap();
+    tree.put(5, 5);
+    tree.put(3, 3);
+    tree.put(7, 7);
+    tree.put(2, 2);
+    tree.put(6, 6);
+    tree.put(8, 8);
+    return tree;
+  }
+
+  @Test
+  public void testToString() {
+    System.out.println(createTemplateTree().toString());
+
+    String string = createTemplateTree().toString();
+    assertEquals("(((2)3)5((6)7(8)))", string);
+  }
+
+  @Test
+  public void testToStringEmpty() {
+    String string = ((BinarySearchTree) createMap()).toString();
+    assertEquals("()", string);
   }
 }
